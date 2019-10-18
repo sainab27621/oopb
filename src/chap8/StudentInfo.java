f78843a078f743e7fa78ef51173ad95ef28abf4c
papercut <papercut@172.17.60.87>
2019-10-18 17:02:31 +0700

    commit1

---
+++ src/chap8/StudentInfo.java
@@ -0,0 +1,36 @@
+package chap8;
+
+
+public class StudentInfo {
+    private  String id ;
+    private  String Name;
+    private  String Surname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String Surname) {
+        this.Surname = Surname;
+    }
+    
+    
+    
+}
+
